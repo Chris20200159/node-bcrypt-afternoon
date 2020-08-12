7ec66ccde29f34906d8de6d2e6d73ac638396a45
Chris Vega <catalina@MacBook-Pro.local>
2020-08-12 08:12:04 -0700

    Updated Server files

---
+++ server/controllers/authController.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  register: async (req, res) => {
+    const {username, password, isAdmin} = req.body;
+    let db = req.app.get('db');
+    let result = await db.get_user([username]);
+    let existingUser = result[0];
+      if(existingUser){
+      return res.status(409).send('Username taken');
+      }
+      let salt = bcrypt.genSaltSync(10);
+      let hash = bcrypt.hashSync(password, salt);
+      let registeredUser = await db.register_user([isAdmin, username, hash]);
+      let user = registeredUser[0];
+      req.session.user = {
+        isAdmin: user.is_admin,
+        username: user.username,
+        id: user.id
+      };
+      res.status(201).send(req.session.user)
+  },
+};
